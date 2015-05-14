4cfa1b286f085b99d505aff4f883f5947341f09b
conch76 <conch76@gmail.com>
2015-05-14 22:03:13 +0900

    Add generic protobuf serialize method

--- src/main/java/com/conch/packet/request/BaseRequestPacket.java
+++ src/main/java/com/conch/packet/request/BaseRequestPacket.java
@@ -6,6 +6,7 @@ public class BaseRequestPacket {
 	
 	// trasient로 해야  protubuf가 serialize 안함
 	private transient RequestPacketType packetType;
+	
 
 	public RequestPacketType getPacketType() {
 		return packetType;
